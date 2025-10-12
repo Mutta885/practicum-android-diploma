0ddeeda3fceb099e1c67639765c63c3a94c29889
aorus <frolovcite@yandex.ru>
2025-10-12 12:02:10 +0300

    sharedPreferences

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/api/StorageManager.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.domain.api
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.FilterParameters
+
+interface StorageManager {
+    suspend fun setFilterSetting(filter: FilterParameters)
+    fun getFilterSetting(): Flow<FilterParameters?>
+    suspend fun clearFilterSetting()
+}
