2392a8db53c559deddd1a0146bb5228eb26a64e6
Dimmar90 <dmitriy.mariyevskiy@gmail.com>
2025-10-14 02:57:14 +0300

    add workplace filtration

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RegionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RegionFragment.kt
@@ -31,7 +31,6 @@ class RegionFragment : Fragment(), RegionAdapter.RegionListener {
 
         viewModel.getRegions(countryId)
         observeViewModel()
-
         binding.returnButton.setOnClickListener {
             findNavController().navigateUp()
         }
