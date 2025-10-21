9d0f8a2bc160b886e0beeecb31a2dea248631281
Руслан <66677799@mail.ru>
2025-10-21 15:06:03 +0300

    fix: исправление отступов в SalaryFormatter по правилам detekt

--- app/src/main/java/ru/practicum/android/diploma/domain/models/SalaryFormatter.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/SalaryFormatter.kt
@@ -5,19 +5,19 @@ import java.util.Locale
 
 object SalaryFormatter {
     fun getFormattedSalary(salary: Salary): String {
-         return when {
-             salary.from != null && salary.to != null ->
-             "от ${formatNumber(salary.from)} до ${formatNumber(salary.to)} ${getCurrencySymbol(salary.currency.toString())}"
+        return when {
+            salary.from != null && salary.to != null ->
+                "от ${formatNumber(salary.from)} до ${formatNumber(salary.to)} ${getCurrencySymbol(salary.currency.toString())}"
 
-             salary.from != null ->
-             "от ${formatNumber(salary.from)} ${getCurrencySymbol(salary.currency.toString())}"
+            salary.from != null ->
+                "от ${formatNumber(salary.from)} ${getCurrencySymbol(salary.currency.toString())}"
 
-             salary.to != null ->
-            "до ${formatNumber(salary.to)} ${getCurrencySymbol(salary.currency.toString())}"
+            salary.to != null ->
+                "до ${formatNumber(salary.to)} ${getCurrencySymbol(salary.currency.toString())}"
 
-             else -> "Зарплата не указана"
-            }.trim()
-         }
+            else -> "Зарплата не указана"
+        }.trim()
+    }
 
     private fun formatNumber(number: Int): String {
         return String.format(Locale.getDefault(), "%,d", number).replace(',', ' ')
@@ -25,12 +25,12 @@ object SalaryFormatter {
 
     private fun getCurrencySymbol(currencyCode: String, locale: Locale = Locale.getDefault()): String {
         var currencySymbol = ""
-         currencySymbol = when (currencyCode) {
-             "RUB" -> "₽"
-             "SEK" -> "KR"
-             "SGD" -> "S$"
-             else -> Currency.getInstance(currencyCode).getSymbol(locale)
-             }
-         return currencySymbol
-       }
+        currencySymbol = when (currencyCode) {
+            "RUB" -> "₽"
+            "SEK" -> "KR"
+            "SGD" -> "S$"
+            else -> Currency.getInstance(currencyCode).getSymbol(locale)
+        }
+        return currencySymbol
+    }
 }
