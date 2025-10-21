67bf1300156f730036c28c8c20736ae9583d3a87
aorus <frolovcite@yandex.ru>
2025-10-21 23:03:35 +0300

    paddingi

--- app/src/main/java/ru/practicum/android/diploma/ui/activity/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/activity/RootActivity.kt
@@ -8,10 +8,12 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+    private var bottomNavigationView: BottomNavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,15 +22,19 @@ class RootActivity : AppCompatActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, if(systemBars.bottom > 0 && bottomNavigationView?.isVisible != false) {
+                0
+            } else {
+                systemBars.bottom
+            })
             insets
         }
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.containerView) as NavHostFragment
         val navController = navHostFragment.navController
-        val bottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView = binding.bottomNavigationView
         val line = binding.line
-        bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView?.setupWithNavController(navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.filtrationFragment,
@@ -37,12 +43,12 @@ class RootActivity : AppCompatActivity() {
                 R.id.countryFragment,
                 R.id.workPlaceFragment,
                 R.id.industryFragment -> {
-                    bottomNavigationView.isVisible = false
+                    bottomNavigationView?.isVisible = false
                     line.isVisible = false
                 }
 
                 else -> {
-                    bottomNavigationView.isVisible = true
+                    bottomNavigationView?.isVisible = true
                     line.isVisible = true
                 }
             }
