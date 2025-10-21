1e3e1f55f72d9f3091acfc4a4c9fea0784afb078
Руслан <66677799@mail.ru>
2025-10-21 14:50:38 +0300

    fix

--- app/src/main/java/ru/practicum/android/diploma/domain/models/SalaryFormatter.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/SalaryFormatter.kt
@@ -1,24 +1,36 @@
 package ru.practicum.android.diploma.domain.models
 
+import java.util.Currency
 import java.util.Locale
 
 object SalaryFormatter {
     fun getFormattedSalary(salary: Salary): String {
-        return when {
-            salary.from != null && salary.to != null ->
-                "от ${formatNumber(salary.from)} до ${formatNumber(salary.to)} ${salary.currency}"
+         return when {
+             salary.from != null && salary.to != null ->
+             "от ${formatNumber(salary.from)} до ${formatNumber(salary.to)} ${getCurrencySymbol(salary.currency.toString())}"
 
-            salary.from != null ->
-                "от ${formatNumber(salary.from)} ${salary.currency}"
+             salary.from != null ->
+             "от ${formatNumber(salary.from)} ${getCurrencySymbol(salary.currency.toString())}"
 
-            salary.to != null ->
-                "до ${formatNumber(salary.to)} ${salary.currency}"
+             salary.to != null ->
+            "до ${formatNumber(salary.to)} ${getCurrencySymbol(salary.currency.toString())}"
 
-            else -> "Зарплата не указана"
-        }.trim()
-    }
+             else -> "Зарплата не указана"
+            }.trim()
+         }
 
     private fun formatNumber(number: Int): String {
-        return String.format(Locale.getDefault(), "%,d", number).replace(',', ' ')
+        return String.format(Locale.getDefault(), "%,d", number).replace(',', ' ')
     }
+
+    private fun getCurrencySymbol(currencyCode: String, locale: Locale = Locale.getDefault()): String {
+        var currencySymbol = ""
+         currencySymbol = when (currencyCode) {
+             "RUB" -> "₽"
+             "SEK" -> "KR"
+             "SGD" -> "S$"
+             else -> Currency.getInstance(currencyCode).getSymbol(locale)
+             }
+         return currencySymbol
+       }
 }
