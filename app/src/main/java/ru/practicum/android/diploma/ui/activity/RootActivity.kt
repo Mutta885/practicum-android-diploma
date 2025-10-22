386c9d485c15b6d7e5491a4acfc58ca080d25195
Руслан <66677799@mail.ru>
2025-10-22 11:02:42 +0300

    fix

--- app/src/main/java/ru/practicum/android/diploma/ui/activity/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/activity/RootActivity.kt
@@ -22,12 +22,16 @@ class RootActivity : AppCompatActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, if (systemBars.bottom > 0 && bottomNavigationView?.isVisible != false) 
-            {
-                0
-            } else {
-                systemBars.bottom
-            })
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                if (systemBars.bottom > 0 && bottomNavigationView?.isVisible != false) {
+                    0
+                } else {
+                    systemBars.bottom
+                }
+            )
             insets
         }
         val navHostFragment =
