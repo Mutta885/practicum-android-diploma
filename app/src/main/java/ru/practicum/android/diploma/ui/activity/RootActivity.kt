5a2e60e45962804a68da3eb92a3145ba21978930
urbanrzhev <aorus69reg@hotmail.com>
2025-10-22 08:39:35 +0300

    Update RootActivity.kt

--- app/src/main/java/ru/practicum/android/diploma/ui/activity/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/activity/RootActivity.kt
@@ -22,7 +22,8 @@ class RootActivity : AppCompatActivity() {
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, if(systemBars.bottom > 0 && bottomNavigationView?.isVisible != false) {
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, if (systemBars.bottom > 0 && bottomNavigationView?.isVisible != false) 
+            {
                 0
             } else {
                 systemBars.bottom
